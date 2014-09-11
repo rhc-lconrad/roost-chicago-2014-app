da4dbb154f3a0e18a10d20c26bce72a140707985
Liam Conrad <lconrad@restorehc.com>
2014-09-11 15:58:17 -0500

    unwink

--- src/requirejs-config.js
+++ src/requirejs-config.js
@@ -6,7 +6,7 @@
     paths: {
       backbone: 'bower_components/backbone/backbone',
       jquery: 'bower_components/jquery/dist/jquery',
-      underscore: 'bower_components/lodash/dist/lodash.underscore',
+      underscore: 'bower_components/lodash/dist/lodash.min',
       lodash: 'bower_components/lodash/dist/lodash',
       tmpl: 'bower_components/lodash-template-loader/loader',
       vintagejs: 'bower_components/vintagejs/dist/jquery.vintage',
